264ffbd4dc51a77275d69f470ed2c0379fcbd8cc
wcrbrm <viclopata@gmail.com>
2018-08-02 19:43:18 +0300

    Contract for MyToken - merged into single file

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,19 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 1000000 // the upfront cost of contract deployement??
+    }
+  },
+  rpc: {
+    host: "localhost",
+    port: 8545
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
+
 };
